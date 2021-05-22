ae8ee1481ad19535d517e8573d1fbbb9af77706f
hawkeye1203 <83642467+hawkeye1203@users.noreply.github.com>
2021-05-22 22:12:54 +0200

    Add files via upload

---
+++ Launcher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher
+{
+    class Program
+    {
+
+        public string License { get; set; }
+
+        [ArmDot.Client.VirtualizeCode]
+        static void Main(string[] args)
+        {
+            Program myObj = new Program();
+            myObj.License = "RLUywEa0BwpiKZzxj6sPHI4YR Q3ipOhf1aMm3IzErS/JTR0RnB MKONZjNHOmtNDd027bPDS184H /o5WJibVVL5ncm5AN4Bls1AwV uCs/4A2PLC/DpMm+GSHqQhbpP yKdwz80Iy41NUFlapeLOvGFIt vuH4FjXWfy1RDqLqqrZV8A";
+
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+
+        }
+    }
+}
